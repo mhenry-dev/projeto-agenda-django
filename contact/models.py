1d5e454a515d0d9de62f1fd6a576d75f27005fef
Michael Santos <michael.h.s@hotmail.com>
2026-07-20 17:03:35 -0300

    Registrando meu model na área administrativa do Django( admin.py e register)

--- contact/models.py
+++ contact/models.py
@@ -11,3 +11,5 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
